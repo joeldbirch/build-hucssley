6365a8a376c2d756012e8d1d13d92e5044e9f428
Joel Birch <joeldbirch@gmail.com>
2020-03-03 15:22:47 +1100

    Add postcss autoprefixer and various setup helpers.

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,17 +1,15 @@
 module.exports = {
-  content: ['./src/**/*.svelte'],
+  content: ['./src/**/*.html', './src/**/*.js', './src/**/*.svelte', './src/**/*.vue' ],
   css: ['src/styles/*.css'],
-  // whitelist: ['all-fill-current'],
-  // whitelistPatterns: [/^logged-in/],
-  keyframes: true,
-  fontFace: true,
+  // whitelist: [],
+  // whitelistPatterns: [/^startswiththis/],
   extractors: [
     {
       extractor: function(content) {
         // eslint-disable-next-line no-useless-escape
         return content.match(/[A-Za-z0-9-_&:@<>\/]+/g) || []
       },
-      extensions: ['svelte', 'html', 'js'],
+      extensions: ['html', 'js', 'svelte', 'vue'],
     },
   ],
 }
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,11 @@ module.exports = {
             loader: 'sass-loader',
             options: {
               implementation: require('sass'),
+              // fiber option moved to sassOptions in sass-loader 8.0+.
+              // However, it's no longer needed as sass-loader uses fiber
+              // by default if you have it installed locally (you still
+              // need to install fiber). But here is how you can explicitly
+              // specify it, anyway…
               sassOptions: {
                 fiber: require('fibers'),
               },
